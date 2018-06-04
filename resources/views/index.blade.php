07c136530315a99046634e21a1bc71f469fea636
maxflex <makcyxa-k@yandex.ru>
2018-06-04 11:36:06 +0300

    style update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,244 +39,246 @@
     </div>
 
     <div
-    	class="program">
-    	<h2>
-    		Программы суранатного материнства
-    	</h2>
+		class="program">
+		<h2>
+			Программы суранатного материнства
+		</h2>
 
-    	<ul
-    		class="program-list">
+		<ul
+			class="program-list">
 
-    		<li
-    			class="program-list__item program-list__item_first">
-    			<div
-    				class="program-list__name">
-    				программа
-    			</div>
-    			<h2>
-    				<a
-    					title="Расширенная"
-    					href="#">
-    					Сопровождение
-    				</a>
-    			</h2>
+			<li
+				class="program-list__item program-list__item_first">
+				<div
+					class="program-list__name">
+					программа
+				</div>
+				<h2>
+					<a
+						title="Расширенная"
+						href="#">
+						Сопровождение
+					</a>
+				</h2>
 
-    			<div
-    				class="program-list__text">
-    				это программа суррогатного материнства полного цикла от подбора суррогатной мамы до получения свидетельства о рождении ребёнка. Весь этот нелёгкий путь мы пройдём вместе, обеспечив сурмаме максимальный комфорт. В её стоимость уже включено всё необходимое. Мы никогда не экономим на здоровье наших сурмам, обеспечивая их всем, что потребуется в процессе вынашивания беременности.
-    			</div>
+				<div
+					class="program-list__text">
+					это программа суррогатного материнства полного цикла от подбора суррогатной мамы до получения свидетельства о рождении ребёнка. Весь этот нелёгкий путь мы пройдём вместе, обеспечив сурмаме максимальный комфорт. В её стоимость уже включено всё необходимое. Мы никогда не экономим на здоровье наших сурмам, обеспечивая их всем, что потребуется в процессе вынашивания беременности.
+				</div>
 
-    			<div
-    				class="program-list__price">
-    				от 2 518 000 рублей
-    			</div>
+				<div
+					class="program-list__price">
+					от 2 518 000 рублей
+				</div>
 
-    			<a
-    				class="button button_bg button_right-arrow"
-    				href="#"
-    				title="Подробнее">
-    				Подробнее
-    			</a>
-    		</li>
+				<a
+					class="button button_bg button_right-arrow"
+					href="#"
+					title="Подробнее">
+					Подробнее
+				</a>
+			</li>
 
-    		<li
-    			class="program-list__item">
-    			<div
-    				class="program-list__name">
-    				программа
-    			</div>
-    			<h2>
-    				<a
-    					title="Расширенная"
-    					href="#">
-    					Расширенная
-    				</a>
-    			</h2>
+			<li
+				class="program-list__item">
+				<div
+					class="program-list__name">
+					программа
+				</div>
+				<h2>
+					<a
+						title="Расширенная"
+						href="#">
+						Расширенная
+					</a>
+				</h2>
 
-    			<a
-    				class="button button_right-arrow"
-    				href="#"
-    				title="Подробнее">
-    				Подробнее
-    			</a>
-    		</li>
+				<a
+					class="button button_right-arrow"
+					href="#"
+					title="Подробнее">
+					Подробнее
+				</a>
+			</li>
 
-    		<li
-    			class="program-list__item">
-    			<div
-    				class="program-list__name">
-    				программа
-    			</div>
-    			<h2>
-    				<a
-    					title="Премиум"
-    					href="#">
-    					Премиум
-    				</a>
-    			</h2>
+			<li
+				class="program-list__item">
+				<div
+					class="program-list__name">
+					программа
+				</div>
+				<h2>
+					<a
+						title="Премиум"
+						href="#">
+						Премиум
+					</a>
+				</h2>
 
-    			<a
-    				class="button button_right-arrow"
-    				href="#"
-    				title="Подробнее">
-    				Подробнее
-    			</a>
-    		</li>
+				<a
+					class="button button_right-arrow"
+					href="#"
+					title="Подробнее">
+					Подробнее
+				</a>
+			</li>
 
-    		<li
-    			class="program-list__item">
-    			<div
-    				class="program-list__name">
-    				программа
-    			</div>
-    			<h2>
-    				<a
-    					title="NEW"
-    					href="#">
-    					NEW
-    				</a>
-    			</h2>
+			<li
+				class="program-list__item">
+				<div
+					class="program-list__name">
+					программа
+				</div>
+				<h2>
+					<a
+						title="NEW"
+						href="#">
+						NEW
+					</a>
+				</h2>
 
-    			<a
-    				class="button button_right-arrow"
-    				href="#"
-    				title="Подробнее">
-    				Подробнее
-    			</a>
-    		</li>
+				<a
+					class="button button_right-arrow"
+					href="#"
+					title="Подробнее">
+					Подробнее
+				</a>
+			</li>
 
-    		<li
-    			class="program-list__item">
-    			<div
-    				class="program-list__name">
-    				программа
-    			</div>
-    			<h2>
-    				<a
-    					title="VIP"
-    					href="#">
-    					VIP
-    				</a>
-    			</h2>
+			<li
+				class="program-list__item">
+				<div
+					class="program-list__name">
+					программа
+				</div>
+				<h2>
+					<a
+						title="VIP"
+						href="#">
+						VIP
+					</a>
+				</h2>
 
-    			<a
-    				class="button button_right-arrow"
-    				href="#"
-    				title="Подробнее">
-    				Подробнее
-    			</a>
-    		</li>
-    	</ul>
+				<a
+					class="button button_right-arrow"
+					href="#"
+					title="Подробнее">
+					Подробнее
+				</a>
+			</li>
+		</ul>
 
-    </div>
+	</div>
 
-    <div
-    	class="steps">
-    	<div
-    		class="steps__inner">
-    		<h2>
-    			Этапы суранатного материнства
-    		</h2>
+    <div class="full-width steps-full-width">
+        <div
+        	class="steps">
+        	<div
+        		class="steps__inner">
+        		<h2>
+        			Этапы суранатного материнства
+        		</h2>
 
-    		<ul
-    			class="steps-list">
-    			<li
-    				class="steps-list__item">
-    				<div
-    					class="steps-list__icon steps-list__icon_icon-1">
-    				</div>
-    				<div
-    					class="steps-list__title">
-    					Звонок в Компанию, назначение даты и времени консультации.
-    				</div>
-    				<div
-    					class="steps-list__date">
-    					1 день
-    				</div>
-    			</li>
-    			<li
-    				class="steps-list__item">
-    				<div
-    					class="steps-list__icon steps-list__icon_icon-2">
-    				</div>
-    				<div
-    					class="steps-list__title">
-    					Очная консультация
-    				</div>
-    				<div
-    					class="steps-list__date">
-    					2-3 дня
-    				</div>
-    			</li>
-    			<li
-    				class="steps-list__item">
-    				<div
-    					class="steps-list__icon steps-list__icon_icon-3">
-    				</div>
-    				<div
-    					class="steps-list__title">
-    					Медицинская консультация
-    				</div>
-    				<div
-    					class="steps-list__date">
-    					4 дня
-    				</div>
-    			</li>
-    			<li
-    				class="steps-list__item">
-    				<div
-    					class="steps-list__icon steps-list__icon_icon-4">
-    				</div>
-    				<div
-    					class="steps-list__title">
-    					Заключение контракта
-    				</div>
-    				<div
-    					class="steps-list__date">
-    					1 день
-    				</div>
-    			</li>
-    			<li
-    				class="steps-list__item">
-    				<div
-    					class="steps-list__icon steps-list__icon_icon-5">
-    				</div>
-    				<div
-    					class="steps-list__title">
-    					Представление базы суррогатных матерей
-    				</div>
-    				<div
-    					class="steps-list__date">
-    					1 день
-    				</div>
-    			</li>
-    			<li
-    				class="steps-list__item">
-    				<div
-    					class="steps-list__icon steps-list__icon_icon-6">
-    				</div>
-    				<div
-    					class="steps-list__title">
-    					Оплата контракта
-    				</div>
-    				<div
-    					class="steps-list__date">
-    					1 день
-    				</div>
-    			</li>
-    		</ul>
+        		<ul
+        			class="steps-list">
+        			<li
+        				class="steps-list__item">
+        				<div
+        					class="steps-list__icon steps-list__icon_icon-1">
+        				</div>
+        				<div
+        					class="steps-list__title">
+        					Звонок в Компанию, назначение даты и времени консультации.
+        				</div>
+        				<div
+        					class="steps-list__date">
+        					1 день
+        				</div>
+        			</li>
+        			<li
+        				class="steps-list__item">
+        				<div
+        					class="steps-list__icon steps-list__icon_icon-2">
+        				</div>
+        				<div
+        					class="steps-list__title">
+        					Очная консультация
+        				</div>
+        				<div
+        					class="steps-list__date">
+        					2-3 дня
+        				</div>
+        			</li>
+        			<li
+        				class="steps-list__item">
+        				<div
+        					class="steps-list__icon steps-list__icon_icon-3">
+        				</div>
+        				<div
+        					class="steps-list__title">
+        					Медицинская консультация
+        				</div>
+        				<div
+        					class="steps-list__date">
+        					4 дня
+        				</div>
+        			</li>
+        			<li
+        				class="steps-list__item">
+        				<div
+        					class="steps-list__icon steps-list__icon_icon-4">
+        				</div>
+        				<div
+        					class="steps-list__title">
+        					Заключение контракта
+        				</div>
+        				<div
+        					class="steps-list__date">
+        					1 день
+        				</div>
+        			</li>
+        			<li
+        				class="steps-list__item">
+        				<div
+        					class="steps-list__icon steps-list__icon_icon-5">
+        				</div>
+        				<div
+        					class="steps-list__title">
+        					Представление базы суррогатных матерей
+        				</div>
+        				<div
+        					class="steps-list__date">
+        					1 день
+        				</div>
+        			</li>
+        			<li
+        				class="steps-list__item">
+        				<div
+        					class="steps-list__icon steps-list__icon_icon-6">
+        				</div>
+        				<div
+        					class="steps-list__title">
+        					Оплата контракта
+        				</div>
+        				<div
+        					class="steps-list__date">
+        					1 день
+        				</div>
+        			</li>
+        		</ul>
 
-    		<div
-    			class="align-center">
-    			<a
-    				href="#"
-    				title="Смотреть подробнее"
-    				class="button button_right-arrow">
-    				Смотреть подробнее
-    			</a>
-    		</div>
+        		<div
+        			class="align-center">
+        			<a
+        				href="#"
+        				title="Смотреть подробнее"
+        				class="button button_right-arrow">
+        				Смотреть подробнее
+        			</a>
+        		</div>
 
-    	</div>
+        	</div>
+        </div>
     </div>
 
     <div
@@ -286,22 +288,9 @@
     		Статистика сурагантного <br>
     		материнства
     	</h2>
-
-    	<ul
-    		class="stats-menu">
-    		<li
-    			class="stats-menu__item stats-menu__item_active stats-menu__item_rus">
-    			В России
-    		</li>
-    		<li
-    			class="stats-menu__item stats-menu__item_world">
-    			В мире
-    		</li>
-    	</ul>
-
     	<div
     		class="stats__chart">
-    		<img
+    		<img style='width: 100%'
     			src="pictures/img2.jpg"
     			alt="IMG"
     		/>
